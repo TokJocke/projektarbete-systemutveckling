c7e2328c2a2b4365fe0d45f0345047d61502d812
Joakim <j-pf@live.se>
2021-02-15 14:14:08 +0100

    renderProducts done!

--- api/repositories/productRepo.php
+++ api/repositories/productRepo.php
@@ -1,24 +1,39 @@
 <?php 
     require "../handlers/dbHandler.php";
-
+    require "../classes/productClass.php";
    
-     function getAllProducts() {
-        $db = new Database();
-        $db->fetchQuery("SELECT * FROM Product");
-       
+    class ProductRepo {
+        function __construct()  {
+            $this->db = new Database();
+           }
+        //Hämta alla produkter från DB, instasera varje produkt, pusha in i ny array som returneras
+        function getAllProducts() {
+            $allProducts = $this->db->fetchQuery("SELECT * FROM Product");
+            $productArray = $this->createProductList($allProducts);
+            return $productArray;
+        }
+        //Skapa lista med produktinstanser
+        function createProductList($array) {
+            $productArray = array();
+            foreach ($array as $item) { //Kan uppnås med array_map
+                $product = new Product($item->productName, (int)$item->price, $item->description, $item->img);
+                array_push($productArray, $product);
+            }
+            return $productArray;
+        }
+           
+        function addProduct($name, $price, $description, $unitsInStock, $categoryID) {
+            
+            $query = ('INSERT INTO product (productName, price, description, unitsInStock, categoryID) 
+            VALUES (:productName, :price, :description, :unitsInStock, :categoryID)');
+            $entity = array(':productName' => $name, ':price' => $price, ':description' => $description, 
+            ':unitsInStock' => $unitsInStock, 'categoryID' => $categoryID);
+            
+            $db = new Database();
+            $db->runQuery($query, $entity);
+    
+        } 
     }
-
-     function addProduct($name, $price, $description, $unitsInStock, $categoryID) {
-        
-        $query = ('INSERT INTO product (productName, price, description, unitsInStock, categoryID) 
-        VALUES (:productName, :price, :description, :unitsInStock, :categoryID)');
-        $entity = array(':productName' => $name, ':price' => $price, ':description' => $description, 
-        ':unitsInStock' => $unitsInStock, 'categoryID' => $categoryID);
-        
-        $db = new Database();
-        $db->runQuery($query, $entity);
-
-    } 
 
 
 ?>
--- products.js
+++ products.js
@@ -7,14 +7,16 @@ let body = document.getElementById("indexBody")
 function initSite() {
 	if (body){
         getAllProducts()
-        addProdBtn()
- 	}
+        renderProducts()
+/*         addProdBtn()
+ */ 	}
 }
 
 async function getAllProducts() {
     const response = await makeReq("./api/recievers/productReciever.php", "GET")
 
     console.log(response)
+    return response
 }
 
 async function addProducts() {
@@ -22,6 +24,37 @@ async function addProducts() {
     console.log(response)
 
 }
+
+async function renderProducts() {
+
+    let allProducts = await getAllProducts()
+    let productWrapper = document.getElementById("testDiv")
+    console.log("in render" , allProducts)
+
+    allProducts.forEach(product => {
+        let productDiv = document.createElement("div")
+        let productTitle = document.createElement("h2")
+        let productDesc = document.createElement("p")
+        let productPrice = document.createElement("h3")
+        let productImg = document.createElement("img")
+
+        productTitle.innerText = product.name
+        productDesc.innerText = product.description
+        productPrice.innerText = product.price + "kr"
+        productImg.src = "./assets/products/" + product.img
+        productDiv.className = "productBox"
+
+        productDiv.append(productTitle, productDesc, productPrice, productImg)
+        productWrapper.append(productDiv)
+
+    });
+
+
+
+}
+
+
+
 
 
 async function addProdBtn() {
