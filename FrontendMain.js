69ef5361cf1ad501a8f5a95e64e5bc8f5a6b3f7a
robinhlgrsn <robinholgersson91@gmail.com>
2021-02-19 13:39:30 +0100

    fixed css layout

--- FrontendMain.js
+++ FrontendMain.js
@@ -4,9 +4,29 @@ const login = document.getElementById("login")
 const cancel = document.getElementById("cancel")
 const userLogin = document.getElementById("userLogin")
 const loginForm = document.getElementById("logInForm")
-const background = document.getElementById("testBackground")
+const background = document.getElementById("popupBackground")
+const registerBtn = document.getElementById("registerBtn")
+
+registerBtn.addEventListener("click", () => {
+  loginForm.innerHTML = ""
+  loadRegForm()
+})
 
 
+function loadRegForm(){
+  const inputOne = document.createElement("input")
+  const inputtwo = document.createElement("input")
+  const inputthree = document.createElement("input")
+  const inputfour = document.createElement("input")
+  const head = document.createElement("h1")
+  head.innerText = "Registrera ny kund"
+  inputOne.placeholder = "Namn"
+  inputtwo.placeholder = "Mail"
+  inputthree.placeholder = "Adress"
+  inputfour.placeholder = "zip" 
+
+  loginForm.append(head, inputOne,inputtwo,inputthree,inputfour)
+}
 
 window.onclick = function(event) {
     if (event.target == background) {
