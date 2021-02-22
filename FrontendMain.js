1cb4c5aa796dcfeacddc89a2a6f285f30ff79c9f
robinhlgrsn <robinholgersson91@gmail.com>
2021-02-22 10:01:16 +0100

    styling fxed

--- FrontendMain.js
+++ FrontendMain.js
@@ -1,5 +1,7 @@
 
+
 let body = document.getElementById("indexBody")
+const formUser = document.getElementById("formUser")
 const login = document.getElementById("login")
 const cancel = document.getElementById("cancel")
 const userLogin = document.getElementById("userLogin")
@@ -9,50 +11,159 @@ const registerBtn = document.getElementById("registerBtn")
 document.getElementById("menuBtn").onclick = function() {menuToggle(this)};
 const cartBtn = document.getElementById("cartBtn")
 
-registerBtn.addEventListener("click", () => {
-  loginForm.innerHTML = ""
+/* registerBtn.addEventListener("click", () => {
+  loginForm.innerHTML =
   loadRegForm()
-})
+}) */
 
 /** menyknapp toggle */
 function menuToggle(x) {
   x.classList.toggle("change");
 } 
-
+ 
 function loadRegForm(){
-  const inputOne = document.createElement("input")
-  const inputtwo = document.createElement("input")
-  const inputthree = document.createElement("input")
-  const inputfour = document.createElement("input")
+  const checkboxDiv = document.createElement("div")
+  const inputMail = document.createElement("input")
+  const inputUsername = document.createElement("input")
+  const inputPassword = document.createElement("input")
+  const inputName = document.createElement("input")
+  const inputAdress = document.createElement("input")
+  const inputZip = document.createElement("input")
+  const inputPhone = document.createElement("input")
+  const newsletter = document.createElement("input")
   const head = document.createElement("h1")
+  const text = document.createElement("p")
+  const btn = document.createElement("div")
   head.innerText = "Registrera ny kund"
-  inputOne.placeholder = "Namn"
-  inputtwo.placeholder = "Mail"
-  inputthree.placeholder = "Adress"
-  inputfour.placeholder = "zip" 
+  inputMail.placeholder = "Mail"
+  inputUsername.placeholder = "Användarnamn"
+  inputPassword.placeholder = "Lösenord"
+  inputName.placeholder = "Namn"
+  inputAdress.placeholder = "Adress" 
+  inputZip.placeholder = "Postnummer" 
+  inputPhone.placeholder = "Telefon" 
+  newsletter.placeholder = "Nyhetsbrev"
 
-  loginForm.append(head, inputOne,inputtwo,inputthree,inputfour)
+  inputMail.id = "registerEmail"
+  inputUsername.id = "registerUsername"
+  inputPassword.id = "registerPassword"
+  inputName.id = "registerName"
+  inputAdress.id = "registerAdress" 
+  inputZip.id = "registerZim" 
+  inputPhone.id = "registerPhone" 
+  newsletter.id = "cb"
+
+  newsletter.type = "checkbox"
+  
+  checkboxDiv.style.display = "flex"
+  checkboxDiv.style.alignItems = "center"
+
+  btn.innerText = "Registrera"
+  btn.className = "defaultBtn"
+  text.innerText = "Nyhetsbrev"
+
+  btn.addEventListener("click", ()=> { //Registreringsknapp eventlistener
+    formUser.innerHTML = ""
+    text.innerText = "Tack för din registrering"
+    formUser.append(text)
+  })
+  
+  checkboxDiv.append(text,newsletter)
+  
+  formUser.append(head,
+    inputMail,
+    inputUsername,
+    inputPassword,
+    inputName,
+    inputAdress,
+    inputZip,
+    inputPhone,checkboxDiv,
+    btn)
+
+  }
+
+
+function loadSignIn(){
+  const username = document.createElement("input")
+  const password = document.createElement("input")
+  const loginBtn = document.createElement("div")
+  const RegText = document.createElement("p")
+  RegText.innerHTML = "Ej medlem? Registrera dig <b>här</b>"
+  RegText.addEventListener("click", () => {
+    formUser.innerHTML = ""
+    loadRegForm()
+  })
+  
+  username.placeholder = "Användarnamn"
+  password.placeholder = "Lösenord"
+
+  username.id = "signinUsername"
+  password.id = "signinPassword"
+
+  loginBtn.className = "defaultBtn"
+  loginBtn.type = "submit"
+  loginBtn.innerText = "Logga in"
+
+  formUser.append(username,password,loginBtn, RegText)
+
 }
 
 window.onclick = function(event) {
     if (event.target == background) {
       background.style.display = "none";
-      logInForm.style.display = "none"
+      logInForm.style.display = "none";
+      formUser.innerHTML = ""
     }
   }
 
 userLogin.addEventListener("click", () =>{
-    loginForm.style.display = "flex"
+    logInForm.style.display = "flex"
     background.style.display = "flex"
+    loadSignIn()
   })
 
 
 cancel.addEventListener("click", () => {
     logInForm.style.display = "none"
     background.style.display = "none"
+    formUser.innerHTML = ""
 } )
 
 
 login.addEventListener("click", () => {
     window.location.href = "/myPage.html";
 } )
+
+
+/* <!-- Jocke & sebbes divar. Ska flyttas till Robins layout
+
+<div>
+    <div class="log-menu">
+        <h1 class="menu-header">LOG-IN</h1>
+        <input id="signinUsername" placeholder="username"> 
+        <input id="signinPassword" type="password" placeholder="password">
+        <div id="error-div"></div> 
+        <button id="loginButton" type="submit">Sign in</button>
+    </div>
+
+
+
+<div id="testDiv"></div>
+
+    
+    <div class="reg-menu">
+        <h1 class="menu-header">CREATE ACCOUNT</h1>
+        <input id="registerEmail" placeholder="Enter your email">
+        <input id="registerName" placeholder="Whats your name?"> 
+        <input id="registerUsername" placeholder="Choose a username">
+        <input id="registerPassword" type="password" placeholder="Choose your password">
+        <input id="registerZip" placeholder="Enter your zip code">
+        <input id="registerAddress" placeholder="Enter your address">
+        <input id="registerPhone" placeholder="Enter your phone number">
+        <div id="newsletter"><h4>Newsletter</h4><input id="cb" type="checkbox"></div>
+        <div id="errorDiv"></div>  
+        <button id="registerButton" type="submit">Register</button>  
+    </div>
+</div>
+
+ --></input> */
--- api/repositories/userRepo.php
+++ api/repositories/userRepo.php
@@ -1,102 +1,103 @@
-<?php 
-    require "../handlers/dbHandler.php";
-    
+<?php
+require "../handlers/dbHandler.php";
 
-    class userRepo {
-      function __construct()
-      {
-        $this->db = new Database();
-      }
-    }
 
-       function regUser($regName, $regUsername, $regPassword, $email, $regAdress, $regZip, $regPhone, $isAdmin) {
+class userRepo
+{
+  function __construct()
+  {
+    $this->db = new Database();
+  }
+}
 
-          $db = new Database();
-          $qry = "SELECT userName FROM user WHERE userName='$regUsername'";
-          $result = $db->fetchQuery($qry);
-          $db = new Database();
-          $qery = "SELECT email FROM user WHERE email='$email'";
-          $newResult = $db->fetchQuery($qery);
-            
-            if($result == true) { //kollar om username / email redan finns vid reg
-              echo json_encode("Username taken");
-              exit;
-            }
+function regUser($regName, $regUsername, $regPassword, $email, $regAdress, $regZip, $regPhone, $isAdmin)
+{
 
-            if($newResult == true) {
-              echo json_encode("Email taken");
-              exit;
-            }
-         
-              else if($result == false && $newResult == false) { //om inte finns så regga användare.
-  
-                $query = ('INSERT INTO user (name, userName, password, email, address, zipCode, phoneNr, isAdmin) 
+  $db = new Database();
+  $qry = "SELECT userName FROM user WHERE userName='$regUsername'";
+  $result = $db->fetchQuery($qry);
+  $db = new Database();
+  $qery = "SELECT email FROM user WHERE email='$email'";
+  $newResult = $db->fetchQuery($qery);
+
+  if ($result == true) { //kollar om username / email redan finns vid reg
+    echo json_encode("Username taken");
+    exit;
+  }
+
+  if ($newResult == true) {
+    echo json_encode("Email taken");
+    exit;
+  } else if ($result == false && $newResult == false) { //om inte finns så regga användare.
+
+    $query = ('INSERT INTO user (name, userName, password, email, address, zipCode, phoneNr, isAdmin) 
                 VALUES (:name, :userName, :password, :email, :address, :zipCode, :phoneNr, :isAdmin)');
 
-                $hashedPwd = password_hash($regPassword, PASSWORD_DEFAULT);
-            
-                $entity = array(':name' => $regName, ':userName' => $regUsername, ':password' => $hashedPwd, 
-                ':email' => $email, ':address' => $regAdress, ':zipCode' => $regZip, 'phoneNr' => $regPhone, ':isAdmin' => $isAdmin);
-              
-                $db = new Database();
-                $db->runQuery($query, $entity);
+    $hashedPwd = password_hash($regPassword, PASSWORD_DEFAULT);
 
-              }
-                
-        } 
+    $entity = array(
+      ':name' => $regName, ':userName' => $regUsername, ':password' => $hashedPwd,
+      ':email' => $email, ':address' => $regAdress, ':zipCode' => $regZip, 'phoneNr' => $regPhone, ':isAdmin' => $isAdmin
+    );
 
-      function login($un, $pw) {
-       
-        $query = "SELECT userName, password FROM user WHERE userName='".$un."' and password='".$pw."'";
-   
-        $db = new Database();
-        $db->fetchQuery($query);
+    $db = new Database();
+    $db->runQuery($query, $entity);
+  }
+}
 
-        $hashedPw = fetchUserInfo($un);
-          // om $pw == hashat pw och matchar med username == success login.  
-          if(password_verify($pw, $hashedPw) == true) {
-            session_start();
-            $_SESSION["user"] = $un;
-            echo json_encode("Login success"); //startar session här..
-            exit;
-        }
-            else {
-            echo json_encode("Login failed");
-            exit;
-          } 
+function login($un, $pw)
+{
 
-      }   
-        //Funktion för att hämta lösenord och matcha med användare.
-        function fetchUserInfo($username) {
-        $query = "SELECT password FROM user WHERE userName='$username'";
-        $db = new Database();
-        $myResult = $db->fetchQuery($query);
-        return $myResult[0]->password;
-    }
+  $query = "SELECT userName, password FROM user WHERE userName='" . $un . "' and password='" . $pw . "'";
 
-    function signUpNewsletter($username) { //Skicka in userid vid reg, email och namn kan vara null
-      //Om ej inloggad, newsletter reg = namn/email.  
-          $query = ('INSERT INTO newsletter (userId) VALUES (:userId)'); //Glöm ej ändra email/name till NULL i databas.
+  $db = new Database();
+  $db->fetchQuery($query);
 
-          $entity = array(':userId' => fetchuId($username));
-              
-          $db = new Database();
-          $db->runQuery($query, $entity);
-    }
+  $hashedPw = fetchUserInfo($un);
+  // om $pw == hashat pw och matchar med username == success login.  
+  if (password_verify($pw, $hashedPw) == true) {
+    session_start();
+    $_SESSION["user"] = $un;
+    echo json_encode("Login success"); //startar session här..
+    exit;
+  } else {
+    echo json_encode("Login failed");
+    exit;
+  }
+}
+//Funktion för att hämta lösenord och matcha med användare.
+function fetchUserInfo($username)
+{
+  $query = "SELECT password FROM user WHERE userName='$username'";
+  $db = new Database();
+  $myResult = $db->fetchQuery($query);
+  return $myResult[0]->password;
+}
 
-    function fetchuId($username) {
-      $db = new Database();
-      $qery = "SELECT userId FROM user WHERE userName='$username'";
-      $result = $db->fetchQuery($qery);
-      return $result[0]->userId;
-    }
+function signUpNewsletter($username)
+{ //Skicka in userid vid reg, email och namn kan vara null
+  //Om ej inloggad, newsletter reg = namn/email.  
+  $query = ('INSERT INTO newsletter (userId) VALUES (:userId)'); //Glöm ej ändra email/name till NULL i databas.
 
-  
+  $entity = array(':userId' => fetchuId($username));
 
-          
+  $db = new Database();
+  $db->runQuery($query, $entity);
+}
 
-            
-     
+function fetchuId($username)
+{
+  $db = new Database();
+  $qery = "SELECT userId FROM user WHERE userName='$username'";
+  $result = $db->fetchQuery($qery);
+  return $result[0]->userId;
+}
 
 
-?>
+function fetchUsers()
+{
+  $db = new Database();
+  $query = "SELECT * FROM user";
+  $result = $db->fetchQuery($query);
+  return json_encode($result);
+}
